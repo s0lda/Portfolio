dc34da5de07fd8e11020d429a8526f7ff5648730
Jakub <81304425+s0lda@users.noreply.github.com>
2021-04-11 20:09:47 +0100

    Update Dice Rolling Simulator.py

--- Dice Rolling Simulator.py
+++ Dice Rolling Simulator.py
@@ -1,13 +1,38 @@
 import random
 
-number_of_dices = int(input('Please select how many dices do you want to use: '))
+def game():
+    while True:
+        try:
+            number_of_dices = int(input('Please select how many dices do you want to use: '))
+        except ValueError:
+            print('Please enter a number')
+            continue
+        break
 
-rolls = []
-def dice_roll(number_of_dices):
-    for i in range(number_of_dices):
-        dice_roll = random.randint(1, 6)
-        rolls.append(dice_roll)
-        print(dice_roll, end=' ')
+    while True:
+        try:
+            s = int(input('How many sides on your dice? '))
+        except ValueError:
+            print('Number of sides must be a number.')
+            continue
+        break
 
-print(dice_roll(number_of_dices))
-print('\nTotal number is: ',sum(rolls))
+    def dice_roll(number_of_dices):
+        _rolls = []
+        for i in range(number_of_dices):
+            dice_roll = random.randint(1, s)
+            _rolls.append(dice_roll)
+            print(dice_roll, end=' ')
+        return _rolls
+
+
+    rolls = dice_roll(number_of_dices)
+    print('\nTotal number is: ',sum(rolls))
+
+while True:
+    game()
+    restart = input('Do you want to restart? Y/N ')
+    if restart == 'N' or 'n':
+        break
+    elif restart == 'Y' or 'y':
+        continue
