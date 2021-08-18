97af492bf43c7bee6c4e0d646d1d019eab80e485
Jakub <81304425+s0lda@users.noreply.github.com>
2021-08-18 23:44:05 +0100

    Update Magic 8 Ball.py

--- Magic 8 Ball.py
+++ Magic 8 Ball.py
@@ -2,5 +2,5 @@ import random
 answers = ['It is certain.', 'It is decidedly so.', 'Yes.', 'Reply hazy try again.', 'Ask again later.', 
 'Concentrate and ask again.', 'My reply is no.', 'Outlook not so good.']
 
-r = random.randint(1, 9)
+r = random.randint(0, 7)
 print(answers[r])
