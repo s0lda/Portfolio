1076d0f87936a3726f513c0552c18a37e51fda8e
Jakub <soldzik@gmail.com>
2021-08-18 23:45:08 +0100

    Update Magic 8 Ball.py

--- Magic 8 Ball.py
+++ Magic 8 Ball.py
@@ -1,6 +1,23 @@
 import random
-answers = ['It is certain.', 'It is decidedly so.', 'Yes.', 'Reply hazy try again.', 'Ask again later.', 
-'Concentrate and ask again.', 'My reply is no.', 'Outlook not so good.']
 
-r = random.randint(0, 7)
-print(answers[r])
+def eight_ball():
+    answers = ['It is certain.', 'It is decidedly so.', 'Yes.', 'Reply hazy try again.', 'Ask again later.', 
+    'Concentrate and ask again.', 'My reply is no.', 'Outlook not so good.']
+
+
+    print('You can ask multiple questions. To exit type exit.')
+    valid = False
+    while valid == False:
+        try:
+            print('What is your question?')
+            question = input('>> ')
+            if question.lower() == 'exit':
+                valid = True
+                print('Bye Bye.')
+            else:
+                print(answers[random.randint(0, 7)])
+        finally:
+            pass
+
+if __name__ == '__main__':
+    eight_ball()
