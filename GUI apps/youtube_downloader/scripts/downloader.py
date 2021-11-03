adc15a7003e1bda77b73fd6102ea60261024c137
Jakub <soldzik@gmail.com>
2021-11-03 21:13:37 +0000

    .

--- GUI apps/youtube_downloader/scripts/downloader.py
+++ GUI apps/youtube_downloader/scripts/downloader.py
@@ -1,5 +1,4 @@
 from pytube import YouTube
-import os
 from pathlib import Path
 
 class Downloader:
@@ -12,19 +11,14 @@ class Downloader:
         name = video.title
         if MP3BoolVal == True:
             new_name = f'{name}.mp3'
-            video.streams.filter(only_audio=MP3BoolVal).first().download(filename=name, output_path=downloads_path)
+            video.streams.filter(only_audio=MP3BoolVal).first().download(filename=new_name, output_path=downloads_path)
         else:
             new_name = f'{name}.mp4'
             # try to download 720p if not available download whatever is available
             try:
-                video.streams.filter(progressive=True, res='720p').first().download(filename=name, output_path=downloads_path)
+                video.streams.filter(progressive=True, res='720p').first().download(filename=new_name, output_path=downloads_path)
             except AttributeError:
-                video.streams.filter(progressive=True).first().download(filename=name, output_path=downloads_path)
+                video.streams.filter(progressive=True).first().download(filename=new_name, output_path=downloads_path)
 
-        # if video/mp3 already downloaded remove copy
-        try:
-            os.rename(f'{downloads_path}\\{name}', f'{downloads_path}\\{new_name}')
-        except FileExistsError:
-            os.remove(f'{downloads_path}\\{name}')
         return [new_name, downloads_path]
         
